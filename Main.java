a98acb388cb037ae97ecd1c8ca577e85a3151bd0
DavidC22626 <83519503+DavidC22626@users.noreply.github.com>
2021-05-02 10:48:14 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+package mvc;
+import controlador.Controlador;
+import modelo.Pregunta;
+import vista.Preguntas;
+
+public class Main {
+    public static void main(String[] args){
+        
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(VistaPrueba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(VistaPrueba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(VistaPrueba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(VistaPrueba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
+        Pregunta modeloPregunta = new Pregunta();
+        Preguntas vistaPregunta = new Preguntas();
+        Controlador controladorPregunta = new Controlador(modeloPregunta, vistaPregunta);
+        
+        controladorPregunta.iniciarPregunta();
+        vistaPregunta.setVisible(true);
+    }
+}
